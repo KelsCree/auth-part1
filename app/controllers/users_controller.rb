a26e76b4dfc31ce2686126959eb39adf928b8a14
KelsCree <kelseycreehan@gmail.com>
2020-09-22 16:12:42 -0600

    added token auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,14 @@ class UsersController < ApplicationController
     )
     render json: @user
   end
+
+  def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+        @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+        render json: {user: @user, token:@token}
+    else
+      render json: { message: 'Invalid credentials!' }
+    end
+  end
 end
